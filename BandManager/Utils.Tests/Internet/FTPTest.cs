bf5edf0d25da93eead823c6b68c61a7b77f9addb
Grzegorz Pasternak <grzegorz.pasternak.m@gmail.com>
2016-12-18 17:43:07 +0100

    Reorganization of physical folders

--- BandManager/Utils.Tests/Internet/FTPTest.cs
+++ BandManager/Utils.Tests/Internet/FTPTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Utils.Internet;
+﻿using Utils.Internet;
 
-namespace Utils.Tests
+namespace Utils.Tests.Internet
 {
     public class FTPTest
     {
